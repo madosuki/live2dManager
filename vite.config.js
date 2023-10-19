b4f4723b4cc8729b8ccec686244c3efd034ad2e6
madosuki <yurikasamaaikatsu@gmail.com>
2023-10-20 00:13:15 +0900

    add build section to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
-import { defineConfig } from "vite"
+import path from 'path';
+import { defineConfig } from "vite";
 
 export default defineConfig (() => {
   return {
+    root: path.resolve(__dirname, "src"),
+    publicDir: path.resolve(__dirname, "public"),
     resolve: {
       alias: {
         '@': path.resolve(__dirname, 'src'),
         '@CubismWebFramework': path.resolve(__dirname, 'CubismWebFramework/src')
+      }
+    },
+    build: {
+      target: 'es6',
+      outDir: path.resolve(__dirname, "dist"),
+      lib: {
+        entry: '',
+        name: 'live2dManager'
       }
     }
   }
