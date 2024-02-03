9daa7f11a35510cd6858743bafcd22844a485b97
madosuki <yurikasamaaikatsu@gmail.com>
2024-02-03 18:21:38 +0900

    update

--- src/live2dModel.ts
+++ src/live2dModel.ts
@@ -272,6 +272,7 @@ export class Live2dModel extends CubismUserModel {
       this._textureCount++;
 
       if (this._textureCount >= textureCount) {
+        console.log(`textureCount: ${this._textureCount}`);
         this.isCompleteSetup = true;
       }
     };
