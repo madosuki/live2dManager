c37f3f02fd27651f8b96bcb0e6efecee46cb5e96
madosuki <yurikasamaaikatsu@gmail.com>
2024-02-25 21:54:22 +0900

    update

--- src/live2dModel.ts
+++ src/live2dModel.ts
@@ -132,6 +132,7 @@ export class Live2dModel extends CubismUserModel {
   public update(): void {
     const deltaTimeSeconds = LAppPal.getDeltaTime();
     this._userTimeSeconds += deltaTimeSeconds;
+    console.log(this._userTimeSeconds);
 
     this._dragManager.update(deltaTimeSeconds);
     this._dragX = this._dragManager.getX();
