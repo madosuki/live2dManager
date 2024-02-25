5a1cec19c7cbc72a76c99d921819a97bd3f9d7b4
madosuki <yurikasamaaikatsu@gmail.com>
2024-02-25 16:18:11 +0900

    update

--- src/live2dViewer.ts
+++ src/live2dViewer.ts
@@ -279,6 +279,7 @@ export class Live2dViewer {
       const modelKeys = this._models._keyValues;
       console.log(modelKeys);
       for (const i in modelKeys) {
+        console.log(i);
         const projection = new CubismMatrix44();
         const model: Live2dModel = this._models[i];
         if (!model.isCompleteSetup) {
