25495ea57a3936e0176429bbfedd764426e3f5e9
madosuki <yurikasamaaikatsu@gmail.com>
2024-07-27 12:21:08 +0900

    update vite conf

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import path from "path";
 import { defineConfig } from "vite";
 import typescript from "@rollup/plugin-typescript";
+import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 export default defineConfig (() => {
   return {
     root: path.resolve(__dirname, "src"),
@@ -16,6 +19,7 @@ export default defineConfig (() => {
     build: {
       target: "es6",
       outDir: path.resolve(__dirname, "dist"),
+      sourcemap: false,
       lib: {
         entry: "",
         name: "live2dmanager"
