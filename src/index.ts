071234eca62626291b04004d9c95f4ac79720b45
madosuki <yurikasamaaikatsu@gmail.com>
2024-07-30 21:01:45 +0900

    Revert "test comment out"
    This reverts commit fda9a2a81a9e6e14afc62883e8f66a33a496fd26.

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,5 @@
 
 export * from "./touchmanager";
 export * from "./live2dModel";
-// export * from "./live2dMotionSyncModel";
+export * from "./live2dMotionSyncModel";
 export * from "./live2dViewer";
