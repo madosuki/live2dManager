057e9f0c5e96241a5cedce7dfc9816459cea7334
madosuki <yurikasamaaikatsu@gmail.com>
2024-10-12 20:28:58 +0900

    update

--- src/live2dModel.ts
+++ src/live2dModel.ts
@@ -814,5 +814,6 @@ export class Live2dModel extends CubismUserModel {
     this.lipSyncWeight = 0.8;
     this.manualClosedEye = false;
     this.motionFileList = [];
+    this.motionRecord = {};
   }
 }
