347f5d55a94c2dd4a24548960b936a360c52e495
madosuki <yurikasamaaikatsu@gmail.com>
2024-10-12 20:35:59 +0900

    update

--- src/live2dModel.ts
+++ src/live2dModel.ts
@@ -478,11 +478,13 @@ export class Live2dModel extends CubismUserModel {
       const groupName = this._modelSetting.getMotionGroupName(i);
       this._allMotionCount += this._modelSetting.getMotionCount(groupName);
 
+      /*
       for (let motionIndex = 0; motionIndex < this._modelSetting.getMotionCount(groupName); ++motionIndex) {
         const motionFileName = this._modelSetting.getMotionFileName(groupName, motionIndex);
         this.motionRecord[motionFileName] = [groupName, motionIndex];
         this.motionFileList.push(motionFileName);
       }
+      */
 
       if (isPreloadMotion) {
         await this.preLoadMotionGroup(groupName);
