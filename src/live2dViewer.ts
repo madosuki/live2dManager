d464a80ba66e41e5a8ac09e4aa7698267022cf2f
madosuki <yurikasamaaikatsu@gmail.com>
2024-11-19 18:23:55 +0900

    fix invalid return value

--- src/live2dViewer.ts
+++ src/live2dViewer.ts
@@ -98,7 +98,7 @@ export class Live2dViewer {
   }
 
   public setCurrentModel(key: string): boolean {
-    if (!this._models._keyValues.find((v) => v.first === key)) return;
+    if (!this._models._keyValues.find((v) => v.first === key)) return false;
     
     this.targetCurrentModelKey = key;
     return true;
