26ad94ef58699bbaceda5c82e5db88946dcb29c6
madosuki <yurikasamaaikatsu@gmail.com>
2025-07-08 10:19:09 +0900

    rename

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,4 @@ export * from "./touchmanager";
 export * from "./live2dModel";
 export * from "./live2dMotionSyncModel";
 export * from "./live2dViewer";
-export * from "./live2dmanager";
+export * from "./live2dManager";
