51882ddb1af6be3174e8c446bab1893f33ada677
madosuki <yurikasamaaikatsu@gmail.com>
2026-03-31 23:24:57 +0900

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,28 @@
 import path from "path";
 import { defineConfig } from "vite";
-import typescript from "@rollup/plugin-typescript";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 export default defineConfig (() => {
   return {
-    root: path.resolve(__dirname, "src"),
-    publicDir: path.resolve(__dirname, "public"),
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "src"),
-        "@framework": path.resolve(__dirname, "CubismSdkForWeb/src"),
-        "@motionsyncframework": path.resolve(__dirname, "CubismWebMotionSyncComponents/Framework/src")
-      }
-    },
-    build: {
-      target: "es6",
-      outDir: path.resolve(__dirname, "dist"),
-      sourcemap: false,
-      lib: {
-        entry: "",
-        name: "live2dmanager"
-      }
-    },
-    plugins: [
-      typescript()
-    ]
+      root: path.resolve(__dirname, "src"),
+      publicDir: path.resolve(__dirname, "public"),
+      resolve: {
+          alias: {
+              "@": path.resolve(__dirname, "src"),
+              "@framework": path.resolve(__dirname, "CubismSdkForWeb/src"),
+              "@motionsyncframework": path.resolve(__dirname, "CubismWebMotionSyncComponents/Framework/src")
+          }
+      },
+      build: {
+          target: "es2015",
+          outDir: path.resolve(__dirname, "dist"),
+          sourcemap: false,
+          lib: {
+              entry: path.resolve(__dirname, "src/index.ts"),
+              name: "live2dmanager"
+          }
+      },
   }
 });
